9a6c97647cd842d3f9eecd9f18ab90641c3789d2
Vicko657 <victoriaolusegun@gmail.com>
2025-02-27 21:56:51 +0000

    Updated RSVP functionality

--- src/Giftlist.jsx
+++ src/Giftlist.jsx
@@ -6,12 +6,12 @@ export default function Giftlist() {
     <div id="giftlist" className="giftlist d-flex row align-content-center ">
       <h1 className="text-center">Giftlist</h1>
       <p className="text-center">
-        We would love if you were able to bring a gift, <br /> as we have a
+        We would love if you were able to bring a gift, <br /> as we will have a
         dedicated gift table for the bride. <br />
         <br />
         You will find plenty of the options below:
       </p>
-      <div className="all-buttons d-grid gap-2 d-flex justify-content-center">
+      <div className="all-buttons d-grid gap-2 d-flex justify-content-center mt-3">
         <a
           className="giftlist-btn"
           href="https://www.thingstogetme.com/1602857a45f64"
--- src/RSVP.jsx
+++ src/RSVP.jsx
@@ -9,38 +9,89 @@ export default function RSVP() {
     handleSubmit,
     control,
     reset,
+    setError,
+    clearErrors,
     formState: { errors, isSubmitted },
   } = useForm();
   const [loading, setLoading] = useState(null);
   const [submitted, setSubmitted] = useState(null);
   const [user, setUser] = useState(null);
+  const [existingUsers, setExistingUsers] = useState([]);
+  const [formChanged, setFormChanged] = useState(false);
+
+  useEffect(() => {
+    fetchRSVPs();
+  }, []);
+
+  async function fetchRSVPs() {
+    const { data, error } = await supabase
+      .from("Bridal_Rsvp")
+      .select("first_name, last_name");
+    if (error) {
+      console.error("Error fetching RSVPs:", error);
+    } else {
+      setExistingUsers(data);
+    }
+  }
 
   async function onSubmit(data) {
     setLoading(true);
+
     try {
+      const firstName = data.first_name.trim().toLowerCase();
+      const lastName = data.last_name.trim().toLowerCase();
+
+      const { data: existingUser, error: fetchError } = await supabase
+        .from("Bridal_Rsvp")
+        .select("id")
+        .eq("first_name", firstName)
+        .eq("last_name", lastName)
+        .maybeSingle();
+
+      if (fetchError) {
+        throw fetchError;
+      }
+
+      if (existingUser) {
+        setError("combined_name_error", {
+          type: "manual",
+          message: "This full name has already rsvp'd.",
+        });
+
+        setLoading(false);
+        return;
+      }
+
       const { error } = await supabase
         .from("Bridal_Rsvp")
-        .insert([data])
+        .insert([{ ...data, first_name: firstName, last_name: lastName }])
         .select("first_name, last_name")
         .single();
 
       if (error) throw error;
 
       setSubmitted(true);
+
+      setUser({ first_name: firstName, last_name: lastName });
       reset();
     } catch (error) {
-      setError(error.message);
+      console.error("Error:", error.message);
+      setUser(null);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
-    setUser(data);
   }
 
+  const handleFieldChange = () => {
+    setFormChanged(true);
+    clearErrors("combined_name_error");
+  };
   return (
     <div id="rsvp" className="rsvp-form d-flex row align-content-center">
       {submitted ? (
         <div className="rsvp-submitted text-center">
           <h1 className="text-light m-0 p-0"> Thank You</h1>
-          <p className="fullname text-light pt-4 pb-2 ">
+          <p className="fullname text-light pt-4 pb-2 text-capitalize">
             {user && (
               <>
                 {user.first_name} {user.last_name}
@@ -75,51 +126,63 @@ export default function RSVP() {
             onSubmit={handleSubmit(onSubmit)}
           >
             <div className="col-md-6">
-              <label for="validationCustom01" class="form-label">
-                First Name
-              </label>
+              <label class="form-label">First Name</label>
               <input
                 type="text"
-                {...register("first_name", { required: true })}
+                {...register("first_name", {
+                  required: "Please enter your first name.",
+                })}
                 className={`form-control  ${
                   isSubmitted &&
-                  (errors.first_name ? "border-warning" : "border-Success")
+                  (errors.first_name || errors.combined_name_error
+                    ? "border-warning"
+                    : "border-Success")
                 }`}
                 aria-label="First name"
+                onChange={handleFieldChange}
               />
               {errors.first_name && (
                 <small className="text-warning text-sm">
-                  Please enter your first name.
+                  {errors.first_name.message}
                 </small>
               )}
             </div>
             <div className="col-md-6">
-              <label for="validationCustom02" class="form-label">
-                Surname
-              </label>
+              <label class="form-label">Surname</label>
               <input
                 type="text"
-                {...register("last_name", { required: true })}
+                {...register("last_name", {
+                  required: "Please enter your surname.",
+                })}
                 className={`form-control ${
                   isSubmitted &&
-                  (errors.last_name ? "border-warning" : "border-Success")
+                  (errors.last_name || errors.combined_name_error
+                    ? "border-warning"
+                    : "border-Success")
                 }`}
                 aria-label="Last name"
+                onChange={handleFieldChange}
               />
               {errors.last_name && (
                 <small className="text-warning text-sm">
-                  Please enter your surname.
+                  {errors.last_name.message}
                 </small>
               )}
             </div>
+            {errors.combined_name_error && (
+              <div className="col-12 text-center">
+                <small className="text-warning">
+                  {errors.combined_name_error.message}
+                </small>
+              </div>
+            )}
             <div className="col-12">
-              <label for="validationCustom03" class="form-label">
-                Will you be attending?
-              </label>
+              <label class="form-label">Will you be attending?</label>
               <Controller
                 name="rsvp"
                 control={control}
                 defaultValue="YES"
+                rules={{ required: "Please confirm your attendance." }}
                 render={({ field }) => (
                   <select
                     {...field}
@@ -133,14 +196,14 @@ export default function RSVP() {
                   </select>
                 )}
               />
-              {errors.firt_name && (
+              {errors.rsvp && (
                 <small className="text-warning text-sm">
-                  Please confirm your attendance.
+                  {errors.rsvp.message}
                 </small>
               )}
             </div>
             <div className="col-12">
-              <label for="validationCustom04" class="form-label">
+              <label class="form-label">
                 Do you have any dietary requirements?
               </label>
               <input
@@ -151,23 +214,24 @@ export default function RSVP() {
               />
             </div>
             <div className="col-12">
-              <label for="validationCustom05" class="form-label">
-                Please leave a special message for the Bride
+              <label class="form-label">
+                Please leave a special message for the bride
               </label>
 
               <textarea
-                {...register("message", { required: false })}
+                {...register("message")}
                 class="form-control"
                 id="exampleFormControlTextarea1"
                 rows="3"
                 aria-label="message"
               ></textarea>
             </div>
+
             <div class="col-12 d-flex justify-content-center">
               <button
                 type="submit"
                 class="rsvp-btn btn btn-primary m-0 "
-                disabled={loading}
+                disabled={!formChanged || loading}
               >
                 {loading ? "Submitting..." : "SUBMIT RSVP"}
               </button>
